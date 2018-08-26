291358d30a7c57df6e28543059117538e7df1624
Vladimir Kopytok <kopytokv@gmail.com>
2018-08-26 20:06:57 +0300

    Series 18 in 10 sec

--- linalg.py
+++ linalg.py
@@ -30,7 +30,7 @@ class Domain(object):
         self.numbers = np.linspace(0, length-1, length)
         self.grid = np.empty((length, length))
         self.grid[:] = np.nan
-        # self.grid[0,0] = False
+        self.grid[0,0] = False
 
     def __str__(self):
         return str(self.grid)
@@ -51,16 +51,21 @@ class Domain(object):
     def __iter__(self):
         """ Generate available values from most right column
             with available values """
+        logging.debug("Grid:\n{}".format(self))
         missing_values = self.missing_values()
-        if (~np.isnan(missing_values)).any():
-            position = np.where((~np.isnan(missing_values)).max(0))[0][-1]
+        logging.debug("Iter missing values:\n{}".format(str(missing_values)))
+        missing_mask   = (~np.isnan(missing_values))
+        if missing_mask.any():
+            position = np.where(missing_mask.max(0))[0][-1]
             available = missing_values[:, position]
+            logging.debug("Iter available: {}".format(available))
             for value in available[~np.isnan(available)]:
                 yield value, position
 
     def to_numbers(self):
         """ Convert 2-D domain into 1-D array """
-        return np.dot(self.numbers, self.grid)
+        numbers = np.dot(self.numbers, self.grid)
+        return numbers
 
     def missing_values(self):
         """ Make matrix with only available missing values """
@@ -68,17 +73,28 @@ class Domain(object):
         values = mask * self.numbers.reshape((-1, 1))
         return np.where(mask, values, np.nan)
 
-    def estimate(self, f="min"):
-        """ Estimate min or max available number for each missing postion """
-        clean_bound = np.nan_to_num(self.missing_values()) -\
-            np.nan_to_num(self.to_numbers()) + np.nansum(self.to_numbers())
+    def estimate(self, f="min", how="sum"):
+        """ Estimate min or max (position), if `sum`,
+            (index * position), if `mult`
+            for each missing position """
+        mask = np.isnan(self.grid)
 
-        clean_bound = np.where(np.isnan(self.grid), clean_bound, np.nan)
-        estimate = eval("np.nan%s" % f)(self.missing_values(), 0)
-        all_sum = np.nansum(estimate)
-        return clean_bound + all_sum - estimate
+        if how == "mult":
+            values = np.multiply(*np.mgrid[:self.length, :self.length])
+            missing_values = np.where(mask, values, np.nan)
+            num = np.multiply(self.to_numbers(), self.numbers)
+        else:
+            missing_values = self.missing_values()
+            num = self.to_numbers()
 
-    def prune(self): # TODO
+        clean_bound = np.nan_to_num(missing_values) - \
+            np.nan_to_num(num) + np.nansum(num)
+        clean_bound = np.where(mask, clean_bound, np.nan)
+        est = eval("np.nan%s" % f)(missing_values, 0)
+        all_sum = np.nansum(est)
+        return clean_bound + all_sum - est
+
+    def prune(self):
         """ Prune unfeasible values from domain """
         return prune(self)
 
@@ -87,18 +103,18 @@ class Domain(object):
         results = set()
         for value, position in self:
             temp_domain = copy.deepcopy(self)
+            logging.debug("value: {}, position: {}".format(value, position))
             temp_domain[position] = value
             feasibile = temp_domain.prune()
             if not feasibile:
                 continue
 
             # If no missing values, test and save
-            if not np.isnan(temp_domain.to_numbers()).any():
+            no_missing = not np.isnan(temp_domain.to_numbers()).any()
+            if no_missing:
                 logging.debug("Full series: {}".format(repr(temp_domain)))
                 logging.debug("Magic: {}".format(temp_domain.magic_series()))
-            if not np.isnan(temp_domain.to_numbers()).any() and \
-                    feasibile and \
-                    temp_domain.magic_series():
+            if no_missing and feasibile and temp_domain.magic_series():
                 results.add(repr(temp_domain))
 
             # Save all previous results
@@ -113,9 +129,14 @@ class Domain(object):
         """ Test domain feasibility
             ((b == 1) & (x == v)) | ((b == 0) & (x != v)) """
         values, positions = np.mgrid[:self.length, :self.length]
-        feasibility = np.logical_or(np.isnan(self.grid), np.equal(self.grid,
-            np.equal(values, np.repeat([self.to_numbers()], self.length,
-                axis=0))))
+        feasibility = \
+            np.logical_or(
+                np.isnan(self.grid),
+                np.equal(
+                    self.grid,
+                    np.equal(
+                        values,
+                        np.repeat([self.to_numbers()], self.length, axis=0))))
         return feasibility
 
 
--- linalg_prune.py
+++ linalg_prune.py
@@ -4,9 +4,12 @@ import logging
 import numpy as np
 
 def prune_sum_eq_len(domain):
+    # Add estimate (position * value)
     constraints = [
-        domain.estimate("min") > domain.length,
-        domain.estimate("max") < domain.length,
+        domain.estimate("min", "sum") > domain.length,
+        domain.estimate("max", "sum") < domain.length,
+        domain.estimate("min", "mult") > domain.length,
+        domain.estimate("max", "mult") < domain.length,
     ]
     pad = reduce(lambda x, y: x | y, constraints)
     pad = pad & np.isnan(domain.grid)
@@ -25,8 +28,8 @@ def prune_fill_last_number(domain):
         position = np.nanmax(np.where(
             np.isnan(domain.to_numbers()),
             domain.numbers, # index
-            np.nan))
-        domain.grid[:, position] = np.isnan(domain.grid[:position])
+            np.nan)).astype(int)
+        domain[position] = domain.length - np.nansum(domain.to_numbers())
         return True
     return False
 
@@ -39,12 +42,22 @@ def prune_less_than_possible(domain):
 
 def prune_row_ready(domain):
     """ Decide number at position if already filled corresponding row """
+    logging.debug("numbers:\n{}".format(domain.to_numbers()))
+    logging.debug("isnan(domain.to_numbers()):\n{}".format(np.isnan(domain.to_numbers())))
+    logging.debug("np.isnan(domain.grid).sum(1) == 0:\n{}".format(np.isnan(domain.grid).sum(1) == 0))
+
     ready_rows = np.where(
         np.isnan(domain.to_numbers()),
         np.isnan(domain.grid).sum(1) == 0,
-        np.nan)
+        0)
+    logging.debug("ready_rows:\n{}".format(ready_rows))
+
     row_sum = np.nansum(domain.grid, 1)
+    logging.debug("row_sum:\n{}".format(row_sum))
+
     filler = np.where(ready_rows, row_sum, np.nan)
+    logging.debug("filler:\n{}".format(filler))
+
     for position, value in enumerate(filler):
         if ~np.isnan(value):
             domain[position] = value
@@ -62,7 +75,23 @@ def prune(domain):
     ]
     changed = feasible = True
     while changed and feasible:
-        changed = any([func(domain) for func in constraints])
-        feasible = domain.feasibility_test().all()
+        changed = False
+        not_changed_cnt = 0
+        for func in constraints:
+            not_changed_cnt += 1
+            logging.debug("Domain before {}:\n{}".format(func.__name__, str(domain)))
+            pruned = func(domain)
+            if pruned:
+                not_changed_cnt = 0
+            changed = changed or pruned
+            logging.debug("Domain after {}:\n{}".format(func.__name__, str(domain)))
+            feasible = domain.feasibility_test().all()
+            too_long = not_changed_cnt > len(constraints)
+            filled = np.isnan(domain.grid).sum() == 0
+            logging.debug("Filled:\n{}".format(filled))
+            if too_long or filled or not feasible:
+                changed = False
+                break
         logging.debug("Feasible: {}".format(feasible))
+        logging.debug("Domain:\n%s" % str(domain))
     return feasible
